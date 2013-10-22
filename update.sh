6da8971b149e6dfda47f346cee6a07ad948d38db
Ryan Baumann <ryan.baumann@gmail.com>
2013-10-22 10:23:04 -0400

    Add update script

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget http://atlantides.org/downloads/pleiades/dumps/pleiades-places-latest.csv.gz http://atlantides.org/downloads/pleiades/dumps/pleiades-locations-latest.csv.gz http://atlantides.org/downloads/pleiades/dumps/pleiades-names-latest.csv.gz
+gunzip *.csv.gz
+./pleiades-geojson.rb pleiades-places-latest.csv pleiades-names-latest.csv pleiades-locations-latest.csv
+git add geojson
+git commit -m "$(date '+%Y-%m-%d') GeoJSON update"
+rm -v *.csv
