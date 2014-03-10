83eb82a216db9f6232b109658e7588587369f890
Ryan Baumann <ryan.baumann@gmail.com>
2014-03-10 14:51:09 -0400

    Split update.sh out and add update-cron.sh script with git commands

---
+++ update-cron.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# needed for ssh-agent auth under cron on OS X
+declare -x SSH_AUTH_SOCK=$( find /tmp/launch-*/Listeners -user $(whoami) -type s | head -1 )
+
+git checkout master
+git pull
+./update.sh
+git add geojson
+git add name_index.json
+git commit -m "$(date '+%Y-%m-%d') GeoJSON update"
+git checkout gh-pages
+git pull
+git merge master
+git push
+git checkout master
--- update.sh
+++ update.sh
@@ -3,7 +3,4 @@
 wget http://atlantides.org/downloads/pleiades/dumps/pleiades-places-latest.csv.gz http://atlantides.org/downloads/pleiades/dumps/pleiades-locations-latest.csv.gz http://atlantides.org/downloads/pleiades/dumps/pleiades-names-latest.csv.gz
 gunzip *.csv.gz
 ./pleiades-geojson.rb pleiades-places-latest.csv pleiades-names-latest.csv pleiades-locations-latest.csv
-git add geojson
-git add name_index.json
-git commit -m "$(date '+%Y-%m-%d') GeoJSON update"
 rm -v *.csv
