6afbcb0d72c4a4e7d3ee85b93f52a1b1b8a1e09a
Ryan Baumann <ryan.baumann@gmail.com>
2014-06-09 10:55:52 -0400

    New socket path on Yosemite

--- update-cron.sh
+++ update-cron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # needed for ssh-agent auth under cron on OS X
-declare -x SSH_AUTH_SOCK=$( find /tmp/launch-*/Listeners -user $(whoami) -type s | head -1 )
+declare -x SSH_AUTH_SOCK=$( find /tmp/com.apple.launchd.*/Listeners -user $(whoami) -type s | head -1 )
 
 git checkout master
 git pull
