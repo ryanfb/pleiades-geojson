f985ff579dde1a3f7397171e6267a56bd693989c
Ryan Baumann <ryan.baumann@gmail.com>
2015-04-16 16:43:20 -0400

    Delete master branch, only use gh-pages

--- update-cron.sh
+++ update-cron.sh
@@ -3,14 +3,10 @@
 # needed for ssh-agent auth under cron on OS X
 declare -x SSH_AUTH_SOCK=$( find /tmp/com.apple.launchd.*/Listeners -user $(whoami) -type s | head -1 )
 
-git checkout master
+git checkout .
 git pull
 ./update.sh
 git add geojson
 git add name_index.json
 git commit -m "$(date '+%Y-%m-%d') GeoJSON update"
-git checkout gh-pages
-git pull
-git merge master
 git push
-git checkout master
